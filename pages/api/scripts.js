8c7c9a44e6d72237940ece29cae0273aa92fb475
Wic1k <dragonmarxa3@gmail.com>
2025-12-19 19:07:06 +0200

    Create scripts.js

---
+++ pages/api/scripts.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+    const userAgent = req.headers['user-agent'] || '';
+    const referer = req.headers.referer || '';
+    
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache');
+    
+    if (!userAgent.toLowerCase().includes('roblox') || !referer.includes('culthub.vercel.app')) {
+        return res.redirect(302, 'https://yoxi-hub.ru');
+    }
+    
+    const script = `loadstring(game:HttpGet("https://raw.githubusercontent.com/erxsethis/yoxi/refs/heads/main/loader",true))()`;
+    return res.status(200).send(script);
+}
