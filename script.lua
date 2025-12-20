ab0e78b0213768ea950791528fc3c4ba1e0da239
Wic1k <dragonmarxa3@gmail.com>
2025-12-20 08:35:03 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,3 @@
 if game.GameId == 8166233521 then
-  loadstring(game:HttpGet("culthub.vercel.app/surviveonaraft.lua"))()
+  loadstring(game:HttpGet("https://culthub.vercel.app/surviveonaraft.lua"))()
 end
